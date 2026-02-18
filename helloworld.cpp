129449b353b8a1aa5be8d9a68cd3353a8d8d9da7
lalalifiaa <lalalifia20@gmail.com>
2026-02-18 17:25:31 +0700

    Change: Helloo

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World???????" << endl;
     return 0;
 }
+// This simple C++ program prints "Hello, World!" to the console. It includes the iostream library for input and output operations and uses the standard namespace to avoid having to prefix standard library names with "std::". The main function is the entry point of the program, and it returns 0 to indicate that the program ended successfully.
